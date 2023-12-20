88d2dfd8b722632ffc4c4e60ddd70f65ad4d759a
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-12-20 16:57:37 +0330

    create base entity

---
+++ NadinSoft/Src/Data Base/WebNadianSoft.DataBase.Entities/Base/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace WebNadianSoft.DataBase.Entities;
+
+public record BaseEntity
+{
+    public BaseEntity()
+    {
+        CreateDate = DateTime.Now;
+        Status = 0;
+    }
+
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public DateTime CreateDate { get; set; }
+
+    [Required]
+    public int Status { get; set; }
+}
