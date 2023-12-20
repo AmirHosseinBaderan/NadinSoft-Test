8b733c984272960528120a41691ef945e266d7f0
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-12-20 17:23:14 +0330

    create base services

---
+++ NadinSoft/Src/Services/WebNadianSoft.Service.Implementation/Injection/Injector.cs
@@ -0,0 +1,18 @@
+﻿using Coffee.Services.Implementation.Common.Base;
+using Microsoft.Extensions.DependencyInjection;
+using WebNadianSoft.Service.Abstraction;
+using WebNadianSoft.Service.Implemntation;
+using WebNadianSoft.Service.Services;
+
+namespace WebNadianSoft.Service.Implementation;
+
+public static class Injector
+{
+    public static IServiceCollection AddAppServices(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IBaseCud<>), typeof(BaseCud<>));
+        services.AddScoped(typeof(IBaseQuery<>), typeof(BaseQuery<>));
+
+        return services;
+    }
+}
